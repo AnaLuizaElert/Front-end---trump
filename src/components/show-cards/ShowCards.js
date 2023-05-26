22fac9edbce152e9c568cc6c4f536b445ab990bd
ANA LUIZA ELERT <ana_elert@estudante.sesisenai.org.br>
2023-05-26 16:56:04 -0300

    pagination

--- src/components/show-cards/ShowCards.js
+++ src/components/show-cards/ShowCards.js
@@ -1,72 +1,3 @@
-// import { CardService } from "../../service/CardService";
-// import Pagination from 'react-bootstrap/Pagination';
-// import { useEffect, useState } from "react";
-// import CardComponent from "../card/Card";
-// import { v4 as uuidv4 } from 'uuid';
-// import NavBar from '../nav-bar/Nav';
-// import './ShowCards.css';
-
-// function ShowCards() {
-//   const [cards, setCards] = useState([]);
-//   let active = 1;
-//   let items = [];
-//   let qtyItens = 0;
-
-//   useEffect(() => {
-//     const fetchCards = async () => {
-      
-//       CardService.showAll().then((data) => {
-//         qtyItens = data.length;
-//         console.log(qtyItens);
-//         let number = parseInt(qtyItens / 9);
-//         console.log(number);
-//         do {
-//           number --;
-//           items.push(
-//             <Pagination.Item key={uuidv4} active={number === active}>
-//               {number}
-//             </Pagination.Item>,
-//           );
-//         } while(number != 0);
-//       }); 
-
-//       try {
-//         const response = await CardService.showAll(); 
-//         console.log(response.data)
-//         setCards(response.data);
-//       } catch (error) {
-//         console.error(error);
-//       }
-      
-//     };
-
-//     fetchCards();
-//   }, []);
-
-//   return (
-//     <>
-//       <NavBar />
-//       <div className="show-all-cards-container">
-//         {cards.map((card) => (
-//           <CardComponent key={uuidv4()} card={card} />
-//         ))}
-//       </div>
-//       <div>
-//         <Pagination>{items}</Pagination>
-//         <br />
-
-//         <Pagination size="lg">{items}</Pagination>
-//         <br />
-
-//         <Pagination size="sm">{items}</Pagination>
-//       </div>
-//     </>
-//   );
-// }
-
-// export default ShowCards;
-
-
 import { CardService } from "../../service/CardService";
 import Pagination from 'react-bootstrap/Pagination';
 import { useEffect, useState } from "react";
@@ -87,14 +18,14 @@ function ShowCards() {
         const qtyCards = await CardService.qtyCards();
         const response = await CardService.page(active, 9);
         const data = response.data;
-
-        let number = parseInt(qtyCards / 9);
-        console.log(number);
+        
+        let number = parseInt(qtyCards.data / 9);
+        alert(number)
         do {
           number--;
           items.push(
-            <Pagination.Item key={uuidv4()} active={number === active}>
-              {number}
+            <Pagination.Item key={uuidv4()} active={(number + 1) === active}>
+              {number + 1}
             </Pagination.Item>,
           );
         } while (number !== 0);
@@ -118,12 +49,6 @@ function ShowCards() {
       </div>
       <div>
         <Pagination>{items}</Pagination>
-        <br />
-
-        <Pagination size="lg">{items}</Pagination>
-        <br />
-
-        <Pagination size="sm">{items}</Pagination>
       </div>
     </>
   );
