024b5a548acd62856cf091c2e1b82054c3501c0d
Ana Luiza Elert <107824361+AnaLuizaElert@users.noreply.github.com>
2023-10-23 15:27:31 -0300

    error in login

--- src/service/AuthenticationService.js
+++ src/service/AuthenticationService.js
@@ -3,13 +3,8 @@ const url = "http://localhost:8082/login"
 
 export const AuthenticationService = {   
     login: async (login) => {
-        const header = {
-            "User-Agent": "application/json",
-            "Content-Type": "Insomnia/2023.5.7"
-        }
-
         try {
-            const response = await axios.post(url, login, { header, withCredentials: true })
+            const response = await axios.post(url, login, { withCredentials: true })
             return response.data;
         } catch (error) {
             console.error(error);
